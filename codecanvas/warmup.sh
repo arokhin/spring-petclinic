043ca1686979d2248cca530f58de847f596a7785
Artem Rokhin <artem.rokhin@jetbrains.com>
2024-07-30 11:49:30 +0200

    Use latest plugin versions

--- codecanvas/warmup.sh
+++ codecanvas/warmup.sh
@@ -2,9 +2,9 @@
 set -e
 
 #Install plugins
-./codecanvas/pluginsUtils.sh space "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=499599"
+./codecanvas/pluginsUtils.sh space "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=579479"
 ./codecanvas/pluginsUtils.sh kubernetes "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=503023"
-./codecanvas/pluginsUtils.sh com.intellij.ml.llm "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=520378"
+./codecanvas/pluginsUtils.sh com.intellij.ml.llm "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=579703"
 
 #Set Java options
 export JAVA_OPTIONS='-Xmx1g -XX:+HeapDumpOnOutOfMemoryError'
