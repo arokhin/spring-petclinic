06647c6eecc533927058bde9702b69020dab4ca2
arokhin <artem.rokhin@gmail.com>
2024-08-09 14:52:10 +0200

    Update warmup.sh

--- codecanvas/warmup.sh
+++ codecanvas/warmup.sh
@@ -4,6 +4,7 @@ set -e
 #Install plugins
 ./codecanvas/pluginsUtils.sh space "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=555267"
 ./codecanvas/pluginsUtils.sh com.intellij.ml.llm "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=555384"
+./codecanvas/pluginsUtils.sh de.halirutan.mathematica "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=529524"
 
 #Set Java options
 export JAVA_OPTIONS='-Xmx1g -XX:+HeapDumpOnOutOfMemoryError'
