14e956a8d919d055ed48c5fdf53a76008504f7fe
Artem Rokhin <artem.rokhin@jetbrains.com>
2026-03-02 14:37:52 +0100

    Add IDs for subprojects

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -5,12 +5,14 @@ version = "2025.11"
 project {
 
     subProject {
+        id("Backend_Project")
         name = "Backend"
         StandardPipeline("Backend").registerIn(this)
     }
 
 
     subProject {
+        id("Frontend_Project")
         name = "Frontend"
         StandardPipeline("Frontend").registerIn(this)
     }
